d9cdaae963344283797f3c8f7c9905f16d9b9b89
phamchidung342 <63303733+phamchidung342@users.noreply.github.com>
2023-12-01 10:10:46 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const students = [
+  {
+    id: 2410,
+    name: 'PhDuxng',
+    image: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/f7/f7a662239e8a325a4a801fb13152767128ecab72_full.jpg',
+    email: 'phamchidung3@gmail.com',
+  },
+  {
+    id: 2201,
+    name: 'QuangAnh',
+    image: 'https://cdn.cloudflare.steamstatic.com/steamcommunity/public/images/avatars/e5/e5fe7a0bb7d8aaccf08ba5c367a034b171d09d0c_full.jpg',
+    email: 'quanganh@gmail.com',
+  },
+  // Add more students as needed
+];
+
+const Student = ({ id, name, image, email }) => (
+  <div className="student">
+    <img src={image} alt={name} />
+    <div>
+      <h2>{name}</h2>
+      <p>ID: {id}</p>
+      <p>Email: {email}</p>
+    </div>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Student List</h1>
+      {students.map(student => (
+        <Student key={student.id} {...student} />
+      ))}
     </div>
   );
 }
